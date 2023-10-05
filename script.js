572ebefe89290cbce0783fc756a6d3112e941d13
Manojkumar144 <manojsantha1442000@gmail.com>
2023-10-05 20:44:14 +0530

    Added update functionality

--- script.js
+++ script.js
@@ -4,37 +4,52 @@ var itemList = document.getElementById('items');
 
 // Delete event
 itemList.addEventListener('click', deleteItem);
-itemList.addEventListener('click', editItem);
+itemList.addEventListener('click', updateItem);
 
 function getItems() {
-  axios.get("https://crudcrud.com/api/1d2279ac7feb42a69ab13240aa2a8d70/appointmentData")
+  axios.get("https://crudcrud.com/api/8fd01c1312064d54837ddc636a5da5ee/appointmentData")
     .then((response) => {
-      // Assuming the response.data is an array of items
       const items = response.data;
-
-      // Clear the current list
       itemList.innerHTML = "";
 
       // Iterate through the items and add them to the list
       items.forEach((item) => {
         const li = document.createElement('li');
         li.className = 'list-group-item';
-        
+    
         // Create a delete button
         const deleteBtn = document.createElement('button');
         deleteBtn.className = 'delete';
         deleteBtn.textContent = 'X';
 
         deleteBtn.addEventListener('click', () => {
-          deleteItem(item._id); // Call a function to delete the item by ID
+          deleteItem(item._id); 
         });
-
 
         // Create an edit button
         const editBtn = document.createElement('button');
         editBtn.className = 'edit';
         editBtn.textContent = '-';
-        
+
+        editBtn.addEventListener('click', (e) => {
+          const li = e.target.parentElement;
+          const itemText = li.textContent.trim().split('-');
+          const name = itemText[0];
+          const email = itemText[1];
+
+          document.getElementById("name").value = name;
+          document.getElementById("email").value = email;
+
+          const updatedName = document.getElementById("name").value;
+          const updatedEmail = document.getElementById("email").value;
+          const updatedUser = {
+            name: updatedName,
+            email: updatedEmail
+          };
+          deleteItem(item._id);
+          updateItem(item._id, updatedUser);
+        });
+      
         li.appendChild(document.createTextNode(item.name + '-' + item.email));
         li.appendChild(deleteBtn);
         li.appendChild(editBtn);
@@ -61,10 +76,9 @@ function storeItem(e) {
     email: email
   };
 
-  axios.post("https://crudcrud.com/api/1d2279ac7feb42a69ab13240aa2a8d70/appointmentData", user)
+  axios.post("https://crudcrud.com/api/8fd01c1312064d54837ddc636a5da5ee/appointmentData", user)
     .then((response) => {
       console.log(response);
-      // After successfully adding the item, fetch and display the updated list of items
       getItems();
     })
     .catch((err) => {
@@ -76,16 +90,14 @@ function storeItem(e) {
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("number").value = "";
-
 }
 
 // Remove item
 function deleteItem(itemId) {
   if (confirm('Are You Sure?')) {
-  axios.delete(`https://crudcrud.com/api/1d2279ac7feb42a69ab13240aa2a8d70/appointmentData/${itemId}`)
+  axios.delete(`https://crudcrud.com/api/8fd01c1312064d54837ddc636a5da5ee/appointmentData/${itemId}`)
     .then((response) => {
       console.log(`Item with ID ${itemId} deleted successfully`);
-      // After successfully deleting the item, refresh the list
       getItems();
     })
     .catch((error) => {
@@ -94,26 +106,13 @@ function deleteItem(itemId) {
   }
 }
 
-
-// edit item
-function editItem(e){
-  if (e.target.classList.contains('edit')) {
-    var li = e.target.parentElement;
-    var itemText = li.textContent.trim().split('-');
-
-    var name = itemText[0];
-    var email = itemText[1];
-    if (email.endsWith('X')) {
-      email = email.slice(0, -1);
-  }
-    
-    // Populate the name and email input fields
-    document.getElementById("name").value = name;
-    document.getElementById("email").value = email;
-
-    console.log(email);
-
-    // Remove the list item
-    itemList.removeChild(li);
-}
+//update item
+function updateItem(itemId, updatedUser) {
+  axios.put(`https://crudcrud.com/api/8fd01c1312064d54837ddc636a5da5ee/appointmentData/${itemId}`, updatedUser)
+    .then((response) => {
+      console.log(`Item with ID ${itemId} updated successfully`);
+    })
+    .catch((error) => {
+      console.error(`Error updating item with ID ${itemId}:`, error);
+    });
 }
